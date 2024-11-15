b24dd21c30ac3bf1d30c46ff2ffc7a4eed806b8f
Nikolaj Folleraas <nfolleraas@gmail.com>
2024-11-15 11:55:32 +0100

    Started ItemRepository, ItemViewModel and CreateBookingViewModel

---
+++ GettingRealWPF/ViewModels/CreateBookingViewModel.cs
@@ -0,0 +1,23 @@
+﻿using GettingRealWPF.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingRealWPF.ViewModels
+{
+    public class CreateBookingViewModel
+    {
+        private ItemRepository itemRepository = new ItemRepository();
+        public ObservableCollection<ItemViewModel> ItemsVM { get; set; }
+
+        public CreateBookingViewModel()
+        {
+            ItemsVM = new ObservableCollection<ItemViewModel>();
+            
+            foreach (Item item in ItemsVM)
+        }
+    }
+}
--- GettingRealWPF/ViewModels/ItemViewModel.cs
+++ GettingRealWPF/ViewModels/ItemViewModel.cs
@@ -1,7 +1,21 @@
-﻿namespace GettingRealWPF.ViewModels
+﻿using GettingRealWPF.Models.Classes;
+using GettingRealWPF.Models.Enumerations;
+using System.Reflection.Metadata;
+
+namespace GettingRealWPF.ViewModels
 {
     public class ItemViewModel
     {
+        private Item item;
 
+        public string Name { get; set; }
+        public Status Status { get; set; }
+
+        public ItemViewModel(Item item)
+        {
+            this.item = item;
+            Name = item.Name;
+            Status = item.Status;
+        }
     }
 }
