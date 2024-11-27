1c89374f0388e2f5abd30e94e24d6ee0a2deee2f
averyden <igo408gt@gmail.com>
2024-11-27 23:30:55 +0100

    fix silly little typo :3

--- GettingRealWPF/Models/Repositories/BookingRepository.cs
+++ GettingRealWPF/Models/Repositories/BookingRepository.cs
@@ -5,8 +5,13 @@ namespace GettingRealWPF.Models.Repositories
 {
     public class BookingRepository
     {
-        private List<Booking> bookins = new List<Booking>();
+        private List<Booking> bookings = new List<Booking>();
 
         private readonly string filePath = "bookings.txt"; // i have no fucking clue if we should update our DCD to include this, but it is ABSOLUTELY neccesary or else we cant save!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        public void Add(Booking booking)
+        {
+            bookings.Add(booking);
+        }
     }
 }
