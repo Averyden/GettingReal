b4cf75c80f93cb202e6091c8d19665c783b9c54e
averyden <igo408gt@gmail.com>
2024-11-27 23:42:33 +0100

    implement the basic structure for the createBookingViewModel

--- GettingRealWPF/ViewModels/CreateBookingViewModel.cs
+++ GettingRealWPF/ViewModels/CreateBookingViewModel.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GettingRealWPF.Models.Classes;
+using GettingRealWPF.Models.Repositories;
 
 namespace GettingRealWPF.ViewModels
 {
     public class CreateBookingViewModel
     {
+        private BookingRepository bookingRepo;
+
+        public CreateBookingViewModel(BookingRepository bookingRepository)
+        {
+            bookingRepo = bookingRepository;
+        }
+
+        public void CreateBooking(int id, List<ItemRepository> bookingItems, DateTime startDate, DateTime endDate, User connectedUser)
+        {
+            Booking newBooking = new Booking(id, bookingItems, startDate, endDate, connectedUser);
+            bookingRepo.Add(newBooking);
+        }
     }
 }
