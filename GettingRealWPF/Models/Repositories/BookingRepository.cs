6beff31b342b19ded1a5adf9d3853bd1504002cc
averyden <igo408gt@gmail.com>
2024-11-28 10:21:41 +0100

    Implement a simple saveMethod for saving bookings and writing one singular line for the text file

--- GettingRealWPF/Models/Repositories/BookingRepository.cs
+++ GettingRealWPF/Models/Repositories/BookingRepository.cs
@@ -3,10 +3,13 @@
 using GettingRealWPF.Models.Classes;
 using System.IO;
 
+
 namespace GettingRealWPF.Models.Repositories
 {
     public class BookingRepository
     {
+        StringHelper SH = new StringHelper();
+
         private List<Booking> bookings = new List<Booking>();
 
         private readonly string filePath = "bookings.txt"; // i have no fucking clue if we should update our DCD to include this, but it is ABSOLUTELY neccesary or else we cant save!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -17,6 +20,22 @@ namespace GettingRealWPF.Models.Repositories
             
         }
 
+        public void SaveBooking(Booking b)
+        {
+            // use stringhelper to format the saved string.
+            List<string> bookingInfo = new List<string>();
+
+            bookingInfo.Add(b.Id.ToString());
+            bookingInfo.Add(b.BookingItems.ToString());
+            bookingInfo.Add(b.StartDate.ToString());
+            bookingInfo.Add(b.EndDate.ToString());
+            bookingInfo.Add(b.ConnectedUser.ToString());
+
+            using (StreamWriter sr = new StreamWriter(filePath))
+            {
+                sr.WriteLine(SH.PersistanceFormat(bookingInfo));
+            }
+        }
 
         // we should also maybeeee update DCD with this. idk just trying to look more pro here 😎
        
