012fe3875926752fa4668409c47794a44ad71e7f
averyden <igo408gt@gmail.com>
2024-12-10 20:13:36 +0100

    Add an id static field

--- GettingRealWPF/Models/Classes/Booking.cs
+++ GettingRealWPF/Models/Classes/Booking.cs
@@ -4,15 +4,17 @@ namespace GettingRealWPF.Models.Classes
 {
     public class Booking
     {
+        private static int _id;
+
         public int Id { get; set; }
         public Item BookingItems { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public User ConnectedUser { get; set; }
 
-        public Booking(int id, Item bookingItems, DateTime startDate, DateTime endDate, User connectedUser)
+        public Booking(Item bookingItems, DateTime startDate, DateTime endDate, User connectedUser)
         {
-            Id = id;
+            Id = _id++;
             BookingItems = bookingItems; 
             StartDate = startDate;
             EndDate = endDate;
